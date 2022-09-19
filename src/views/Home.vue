9fb435fb9a81db2252125cb51f3fc630aca953d5
Bam kadayat <bamkadayat@gmail.com>
2022-09-19 22:11:45 +0200

    added scss and fixed home style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,67 @@
 <template>
-  <div class="container">
-    <Counter></Counter><br />
-    <div class="columns">
-      <div class="column is-11">
-        <AddCounter></AddCounter>
-      </div>
-      <div class="column auto">
-        <RemoveCounter></RemoveCounter>
+  <div class="home">
+    <div class="container">
+      <div class="head-section"></div>
+      <div class="content">
+        <img src="../assets/hero.jpg" alt="" />
+        <div class="description">
+          <h1>Bam Kadayat</h1>
+          <p>
+            When I started my career as a developer / designer I always
+            struggled to find the latest trends, news, and best practices in the
+            web development world or at least some developers who can help with
+            that.
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Counter from "../components/Counter.vue";
-import AddCounter from "../components/AddCounter.vue";
-import RemoveCounter from "../components/RemoveCounter.vue";
-
-export default {
-  components: {
-    Counter,
-    AddCounter,
-    RemoveCounter,
-  },
-};
+export default {};
 </script>
+<style lang="scss">
+.home {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  .container {
+    overflow: hidden;
+    .content {
+      width: 100vw;
+      height: 100vh;
+      position: relative;
+      img {
+        width: 100vw;
+        height: 100vh;
+        object-fit: cover;
+        filter: brightness(0.4);
+      }
+      .description {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: #fff;
+        background: rgba(26, 59, 75, 0.2);
+        padding: 8px;
+        border-radius: 5px;
+        border: 1px solid rgba($color: #fff, $alpha: 0.2);
+
+        h1 {
+          font-weight: 700;
+          font-size: 1.8rem;
+          font-family: "poppins";
+        }
+        p {
+          line-height: 1.5;
+          font-size: 18px;
+          font-family: "poppins";
+          font-weight: 300;
+        }
+      }
+    }
+  }
+}
+</style>
